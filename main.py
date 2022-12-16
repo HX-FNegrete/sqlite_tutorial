a60a9d3af492ac0d21f6b1ae7d2d05d88e59e939
FranciscoNegrete3 <fran.negrete@hotmail.com>
2022-12-16 12:10:00 -0300

    third commit

--- main.py
+++ main.py
@@ -6,14 +6,25 @@ conn = sqlite3.connect("estudiantes.db")
 c = conn.cursor()
 
 # Con el execute() lo que vamos a hacer es crear una nueva Base, porque estudiantes.db
-# no la teníamos creada.
-c.execute(
-    """ CREATE TABLE estudiantes (
-    nombre TEXT, 
-    edad INTEGER,
-    estatura REAL
-)"""
-)
+# no la teníamos creada. Luego, la comentamos para cuando ejecutemos de nuevo el script
+# no nos genere una nueva tabla con el mismo nombre
+# c.execute(
+#     """ CREATE TABLE estudiantes (
+#     nombre TEXT,
+#     edad INTEGER,
+#     estatura REAL
+# )"""
+# )
+
+# c.execute("INSERT INTO estudiantes VALUES ('Fran', 28, 1.76)")
+
+# El comando executemany() nos permite crear varias filas a partir de una lista
+lista_estudiantes = [("Messi", 35, 1.69), ("Julian", 22, 1.75), ("Dibu", 28, 1.95)]
+
+# c.executemany("INSERT INTO estudiantes VALUES (?, ?, ?)", lista_estudiantes)
+
+c.execute("SELECT * FROM estudiantes")
+print(c.fetchall())
 
 # Guardar avances, hacemos un commit
 conn.commit()
